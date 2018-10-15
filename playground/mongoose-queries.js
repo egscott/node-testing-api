d32b74f3f26e106ab2a9b4f3d3f4e272a5fac678
E. Scott <erics@salemtube.com>
2018-10-15 12:56:17 -0400

    Add queries playground file

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,23 +1,47 @@
+const { ObjectID } = require("mongodb");
 const { mongoose } = require("./../server/db/mongoose");
 const { Todo } = require("./../server/models/todo");
 
-var id = "6bc1049d194619f85e4467e3";
+const { User } = require("./../server/models/users");
 
-Todo.find({
-  _id: id
-}).then(todos => {
-  console.log("Todos", todos);
-});
+// var id = "6bc1049d194619f85e4467e3";
 
-Todo.findOne({
-  _id: id
-}).then(todo => {
-  console.log("Todos", todo);
-});
+// if (!ObjectID.isValid(id)) {
+//   console.log(`ID not valid`);
+// }
 
-Todo.findById(id).then(todo => {
-  if (!todo) {
-    console.log(`Nope, not there`);
-  }
-  console.log("todo", todo);
-});
+// Todo.find({
+//   _id: id
+// }).then(todos => {
+//   console.log("Todos", todos);
+// });
+
+// Todo.findOne({
+//   _id: id
+// }).then(todo => {
+//   console.log("Todos", todo);
+// });
+
+// Todo.findById(id)
+//   .then(todo => {
+//     if (!todo) {
+//       console.log(`Nope, not there`);
+//     }
+//     console.log("todo", todo);
+//   })
+//   .catch(e => {
+//     console.log(e);
+//   });
+
+var id = "5bc08e155a3d03e84284f62a";
+
+User.findById(id)
+  .then(user => {
+    if (!user) {
+      return console.log(`Nope not here`);
+    }
+    console.log(JSON.stringify(user, undefined, 2));
+  })
+  .catch(e => {
+    console.log(e);
+  });
